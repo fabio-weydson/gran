f94f847c912609d1d1eb66d1d07f6c6f32194fea
fabio-weydson <fabioweydson@gmail.com>
2016-06-25 15:00:18 -0300

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,3 @@
-// Mobionic: Mobile Ionic Framework
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'mobionicApp' is the name of this angular module (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
